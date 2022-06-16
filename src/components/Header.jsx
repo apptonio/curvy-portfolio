6e11df86c2d881a9b8ad915b256c198fc2fdecb5
Antonio725 <ab7719@g.rit.edu>
2022-06-16 18:43:43 +0200

    added header and spacer

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='container'>
+      <h2 className='typed-out'>I'm the Header</h2>
+    </div>
+    
+  )
+}
+
+export default Header
