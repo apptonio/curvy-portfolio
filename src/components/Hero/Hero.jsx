47b0baf165b5e889bbc12ced0c013734749d221c
Antonio725 <ab7719@g.rit.edu>
2022-09-05 01:28:18 +0200

    Add social media icons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import Header from "./Header";
 import "./Hero.css";
 import antonio from "../../assets/images/me-grayscale.jpeg";
+import { SocialIcon } from "react-social-icons";
 
 const Hero = () => {
   return (
     <section id="hero">
-      <div>
+      <div className="h-padding">
         <img src={antonio} alt="Profile Avatar of Antonio Butigan" />
+        <Header />
+        <p className="fadein">A freshly graduated Web & Mobile developer.</p>
+        <hr className="fadein line" />
+        <div className="fadein social-icons">
+        <SocialIcon url="https://www.linkedin.com/in/antonio-butigan/" />
+        <SocialIcon url="https://github.com/Antonio725" />
+        <SocialIcon url="https://antoniodev.contactin.bio" network="email"/>
+        </div>
       </div>
-      <Header />
-      <p className="fadein">A freshly graduated Web & Mobile developer.</p>
-
-      <hr className="line" />
-
-
     </section>
   );
 };
