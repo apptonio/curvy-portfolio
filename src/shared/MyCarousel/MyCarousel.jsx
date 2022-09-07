311e19137bad57d66d0e54aee6b007b45d5647d5
Antonio725 <ab7719@g.rit.edu>
2022-09-07 03:19:51 +0200

    Fix carousel and card position

--- src/shared/MyCarousel/MyCarousel.jsx
+++ src/shared/MyCarousel/MyCarousel.jsx
@@ -1,35 +1,75 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "./MyCarousel.css";
-import "react-multi-carousel/lib/styles.css";
+import WithStyles from "react-multi-carousel/lib/styles.css";
 import Card from "../Card/Card";
 
-const CardCarousel = Carousel.default ? Carousel.default : Carousel;
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
 
 const MyCarousel = () => {
   return (
     <div className="w-full">
-      <CardCarousel className="carousel" responsive={responsive} >
-        <Card />
-        <Card />
-        <Card />
-      </CardCarousel>
-    </div>
+
+    
+
+    <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      containerClass="container-with-dots"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite={false}
+      itemClass=""
+      keyBoardControl
+      minimumTouchDrag={80}
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
+          },
+          items: 3,
+          partialVisibilityGutter: 40,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
+          },
+          items: 1,
+          partialVisibilityGutter: 30,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
+          },
+          items: 2,
+          partialVisibilityGutter: 30,
+        },
+      }}
+      rewind={false}
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={false}
+      sliderClass=""
+      slidesToSlide={1}
+      swipeable
+    >
+      <Card />
+      <Card />
+      <Card />
+      
+    </Carousel>
+     </div>
   );
 };
 
