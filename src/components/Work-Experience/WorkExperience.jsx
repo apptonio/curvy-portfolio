b012b37a73261a1acdf8ad9e6faa05338c5ba487
Antonio725 <ab7719@g.rit.edu>
2022-12-04 22:36:10 +0100

    Update work experiences

--- src/components/Work-Experience/WorkExperience.jsx
+++ src/components/Work-Experience/WorkExperience.jsx
@@ -7,6 +7,7 @@ import Card from "../../shared/Card/Card";
 
 import tutor from "../../assets/images/tutor.jpeg";
 import cinnamon from "../../assets/images/cinnamon.png";
+import notch from "../../assets/images/notch.jpg";
 
 const WorkExperience = () => {
   return (
@@ -18,28 +19,38 @@ const WorkExperience = () => {
       <div className="work-experience-container">
       <div className='svg svg-icon2'></div>
       <MyCarousel
-          card1={
+            card1={
+              <Card
+              image={notch}
+              title={"Junior Flutter Engineer"}
+              date={"Oct 2022 - Present"}
+              location={"Zagreb, Croatia"}
+              description={
+                "Working full-time as a Flutter Engineer @ Notch. Expanding my knowledge through work day by day."
+              }
+            />
+            }
+            card2={
             <Card
               image={tutor}
               title={"Teaching Assistant - Mobile Design"}
+              date={"Jan 2022 - May 2022"}
               location={"Dubrovnik, Croatia"}
               description={
                 "Helped students create websites, hybrid & native apps with HTML, CSS, JS, React, Angular and Flutter."
               }
             />
           }
-          card2={
+          card3={
             <Card
               image={cinnamon}
               title={"React Intern Developer"}
+              date={"May 2021 - Aug 2021"}
               location={"Zagreb, Croatia"}
               description={
                 "Worked on completed version-controlled website projects. Worked with React, Angular, HTML, CSS, Git."
               }
             />
-          }
-          card3={
-            null
           }
         />
       </div>
--- src/shared/Card/Card.jsx
+++ src/shared/Card/Card.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({image, title, location, description}) => {
+const Card = ({image, title, date, location, description}) => {
   return (
     <div id="card" className="grow">
       <img src={image} alt="Card Image" />
       <div className="card-text">
+      
         <h4 className="title">{title}</h4>
-        <p className="location">{location}</p>
+        <p className="tag">{location}</p>
         <p className="description">{description}</p>
+        <p className="tag bottomItem">{date}</p>
       </div>
     </div>
   );
